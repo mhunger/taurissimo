305d5c1f3357e42ddebbbac7f5e7ac1b95a45552
mhunger <v10xC3_#..H>
2017-07-12 14:14:57 +0200

    Remove all the noise and add package.json
    - Removed all angular stuff
    - Isolated the current taurissimo classes for forms
    - created package json for npm package manager
    - current classes can be extended and a class created
    that extends taurus-form data that sets the fields in formData
    - The form service can then be used to save data
    - this will have to go together with a cli command that generates
    this form component for angular, generates the formData class,
    the template with the respective form fields from an entity in the
    backend

--- resource.service.ts
+++ resource.service.ts
@@ -1,5 +1,5 @@
-import {Headers, Http} from "@angular/http";
-import {Injectable} from "@angular/core";
+import {Headers, Http} from '@angular/http';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ResourceService {
